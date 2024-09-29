95161ab58c6ff94edb33ba1c7d5c8db7917e3521
Joost de Lange <post.joostdelange@gmail.com>
2024-09-29 21:22:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cd $HOME
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y wget gpg curl zsh apt-transport-https tmux git jq cmake pkg-config python3 postgresql postgresql-contrib python3
+sudo apt install -y wget gpg curl zsh apt-transport-https tmux git jq cmake pkg-config python3 postgresql postgresql-contrib python3 xclip
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
@@ -16,12 +16,10 @@ chsh -s /bin/zsh
 
 if [ ! -d "$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
   git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-  omz plugin enable zsh-autosuggestions; zsh -i
 fi
 
 if [ ! $(which pnpm) ]; then
-  curl -fsSL https://get.pnpm.io/install.sh | zsh -
-  pnpm env use -g 20
+  curl -fsSL https://get.pnpm.io/install.sh | sh -
 fi
 
 if [ ! $(which aws) ]; then
@@ -33,16 +31,6 @@ fi
 
 if [ ! $(which rustup) ]; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-  source "$HOME/.cargo/env"
-fi
-
-if [ ! $(which alacritty) ]; then
-  git clone https://github.com/alacritty/alacritty.git
-  cargo build --release --manifest-path=alacritty/Cargo.toml
-  sudo cp alacritty/target/release/alacritty /usr/local/bin
-  sudo desktop-file-install $REPOSITORY_PATH/alacritty/Alacritty.desktop
-  sudo update-desktop-database
-  rm -rf alacritty
 fi
 
 if [ ! $(which starship) ]; then
@@ -50,23 +38,29 @@ if [ ! $(which starship) ]; then
 fi
 
 if [ ! $(which google-chrome-stable) ]; then
-  curl -LO https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+  sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 fi
 
 if [ ! $(which code) ]; then
-  curl -LO https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+  sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+  echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
 fi
 
 if [ ! $(which tableplus) ]; then
-  curl -LO https://deb.tableplus.com/debian/22/pool/main/t/tableplus/tableplus_0.1.254_amd64.deb
+  wget -qO - https://deb.tableplus.com/apt.tableplus.com.gpg.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/tableplus-archive.gpg > /dev/null
+  sudo add-apt-repository "deb [arch=amd64] https://deb.tableplus.com/debian/24 tableplus main" -y
 fi
 
-if [ ! $(which google-chrome-stable) ] && [ ! $(which code) ] && [ ! $(which tableplus) ]; then
-  sudo apt install ./google-chrome*.deb ./code*.deb ./tableplus*.deb -y
+if [ ! $(which google-chrome-stable) ] && [ ! $(which code) ] && [ ! $(which tableplus) ] && [ ! $(which alacritty) ]; then
+  sudo apt update
+  sudo apt install google-chrome-stable code tableplus alacritty -y
 fi
 
 if [ ! -d "$HOME/.local/share/fonts/Hack" ]; then
   curl -LO https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Hack.zip
+  mkdir -p $HOME/.local/share/fonts/Hack
   unzip -o Hack.zip -d $HOME/.local/share/fonts/Hack
 fi
 

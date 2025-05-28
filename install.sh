b3c57f6eaa37609d9c27a383aebbd3304f0525d1
Joost de Lange <post.joostdelange@gmail.com>
2025-05-29 01:35:24 +0200

    fix

--- install.sh
+++ install.sh
@@ -49,7 +49,7 @@ if [ ! $(which nvim) ]; then
 fi
 
 if [ ! $(which google-chrome-stable) ]; then
-  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+  wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/google.asc >/dev/null
   sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 fi
 

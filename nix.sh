5722a2533c544ae389d994a102c38ca5e712f914
Joost de Lange <post.joostdelange@gmail.com>
2025-09-01 00:49:42 +0200

    wip: NixOS

---
+++ nix.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
+
+sudo rm -f /etc/nixos/configuration.nix
+rm -f $HOME/.config/home-manager/home.nix
+
+sudo ln -s $(pwd)/nix/configuration.nix /etc/nixos/
+ln -s $(pwd)/nix/home.nix $HOME/.config/home-manager/
+
+cat > $(pwd)/nix/configuration-overrides.nix << EOF
+{ pkgs, ... }:
+
+{
+  users = {
+    users = {
+      $(whoami) = {
+        isNormalUser = true;
+        description = "$(whoami)";
+        extraGroups = ["networkmanager" "wheel" "docker"];
+        shell = pkgs.zsh;
+      };
+    };
+  };
+}
+EOF
+
+cat > $(pwd)/nix/home-overrides.nix << EOF
+{ ... }:
+
+{
+  home.username = "$(whoami)";
+  home.homeDirectory = "/home/$(whoami)";
+  programs.git.extraConfig.user.email = "";
+  programs.git.extraConfig.user.name = "";
+  programs.git.extraConfig.user.signingKey = "";
+}
+EOF
+
+mkdir -p $HOME/.aws && touch $HOME/.aws/current_sso_profile
---
+++ nix/home.nix
@@ -0,0 +1,311 @@
+{ pkgs, ... }:
+
+let
+  tableplusAppImage = pkgs.fetchurl {
+    url = "https://tableplus.com/release/linux/x64/TablePlus-x64.AppImage";
+    sha256 = "sha256-4HIPkWqpIcyycpqs3ELcQZUlgmcXeHxdsJ6gS8YmIAg=";
+  };
+
+  tableplus = pkgs.appimageTools.wrapType2 {
+    name = "tableplus";
+    src = tableplusAppImage;
+    version = "1.0";
+    pname = "tableplus";
+    extraInstallCommands = ''
+      mkdir -p $out/bin
+      ln -s $src $out/bin/tableplus
+    '';
+  };
+
+in
+{
+  imports = [./home-overrides.nix];
+
+  home = {
+    stateVersion = "25.05";
+    packages = [
+      pkgs.wget
+      pkgs.curl
+      pkgs.git
+      pkgs.jq
+      pkgs.nixd
+      pkgs.nil
+      pkgs.oh-my-zsh
+      pkgs.tmux
+      pkgs.starship
+      pkgs.awscli2
+      pkgs.typescript
+      pkgs.esbuild
+      pkgs.nodePackages.ts-node
+      pkgs.nerd-fonts.hack
+      pkgs.pnpm
+      pkgs.docker
+      pkgs.nodejs_22
+      pkgs.appimage-run
+      pkgs.google-chrome
+      pkgs.zed-editor
+      pkgs.ghostty
+      pkgs.neovim
+      tableplus
+      pkgs.stremio
+      pkgs.gnomeExtensions.dash-to-dock
+    ];
+    sessionVariables = {
+      EDITOR = "zed";
+    };
+  };
+
+  xdg = {
+    desktopEntries = {
+      tableplus = {
+        name = "Tableplus";
+        genericName = "Tableplus";
+        exec = "tableplus %U";
+        terminal = false;
+      };
+    };
+  };
+
+  fonts = {
+    fontconfig = { enable = true; };
+  };
+
+  programs = {
+    home-manager = { enable = true; };
+    gpg = { enable = true; };
+    git = {
+      enable = true;
+      extraConfig = {
+        pull = { rebase = false; };
+        push = { default = "current"; };
+        commit = { gpgsign = true; };
+        core = { editor = "nano"; };
+      };
+    };
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion = { enable = true; };
+
+      oh-my-zsh = {
+        enable = true;
+        plugins = ["git"];
+        theme = "robbyrussell";
+      };
+
+      shellAliases = {
+        ww = "cd ~/Projects";
+      };
+      initContent = ''
+        awsswitch() {
+          export AWS_PROFILE=$1;
+          echo $1 > ~/.aws/current_sso_profile;
+        }
+
+        awsswitch $(cat ~/.aws/current_sso_profile)
+      '';
+    };
+    tmux = {
+      enable = true;
+      mouse = true;
+      extraConfig = ''
+      set -g default-terminal "screen-256color"
+      set -ag terminal-overrides ",xterm-256color:Tc"
+      '';
+    };
+    starship = {
+      enable = true;
+    };
+    ghostty = {
+      enable = true;
+      settings = {
+        cursor-style = "bar";
+        background-opacity = 0.9;
+        link-url = true;
+        command = "tmux";
+      };
+    };
+    zed-editor = {
+      enable = true;
+      extensions = ["one-dark-pro" "material-icon-theme" "sql" "vue" "pug" "toml" "dockerfile" "nix"];
+      userKeymaps = [
+        {
+          context = "Workspace";
+          bindings = {
+            "shift shift" = "project_panel::CollapseAllEntries";
+          };
+        }
+        {
+          context = "Editor";
+          bindings = {
+            ctrl-d = "editor::DuplicateLineDown";
+            ctrl-shift-d = ["editor::SelectNext" { "replace_newest" = false; }];
+          };
+        }
+      ];
+      userSettings = {
+        languages = {
+          TypeScript = {
+            language_servers = ["tree-sitter-typescript" "!vtsls" "..."];
+          };
+        };
+        theme = {
+          mode = "system";
+          light = "One Light";
+          dark = "One Dark";
+        };
+        icon_theme = "Material Icon Theme";
+        ui_font_family = "Hack Nerd Font";
+        buffer_font_family = "Hack Nerd Font Mono";
+        ui_font_size = 18;
+        buffer_font_size = 16;
+        tab_size = 2;
+        autosave = "on_focus_change";
+        format_on_save = "off";
+        show_edit_predictions = false;
+        project_panel = {
+          auto_reveal_entries = false;
+        };
+        preview_tabs = {
+          enabled = false;
+        };
+        agent_servers = {
+          gemini = {
+            ignore_system_version = false;
+          };
+        };
+      };
+    };
+  };
+
+  dconf = {
+    settings = {
+      "org/gnome/control-center" = {
+        last-panel = "power";
+        window-state = "(980, 640, false)";
+      };
+      "org/gnome/desktop/app-folders" = {
+        folder-children = [ "Utilities" "YaST" "Pardus" ];
+      };
+      "org/gnome/desktop/app-folders/folders/Pardus" = {
+        categories = [ "X-Pardus-Apps" ];
+        name = "X-Pardus-Apps.directory";
+        translate = true;
+      };
+      "org/gnome/desktop/app-folders/folders/Utilities" = {
+        apps = [ "gnome-abrt.desktop" "gnome-system-log.desktop" "nm-connection-editor.desktop" "org.gnome.baobab.desktop" "org.gnome.Connections.desktop" "org.gnome.DejaDup.desktop" "org.gnome.Dictionary.desktop" "org.gnome.DiskUtility.desktop" "org.gnome.Evince.desktop" "org.gnome.FileRoller.desktop" "org.gnome.fonts.desktop" "org.gnome.Loupe.desktop" "org.gnome.seahorse.Application.desktop" "org.gnome.tweaks.desktop" "org.gnome.Usage.desktop" "vinagre.desktop" ];
+        categories = [ "X-GNOME-Utilities" ];
+        name = "X-GNOME-Utilities.directory";
+        translate = true;
+      };
+      "org/gnome/desktop/app-folders/folders/YaST" = {
+        categories = [ "X-SuSE-YaST" ];
+        name = "suse-yast.directory";
+        translate = true;
+      };
+      "org/gnome/desktop/applications/terminal" = {
+        exec = "ghostty";
+      };
+      "org/gnome/desktop/background" = {
+        color-shading-type = "solid";
+        picture-options = "zoom";
+        picture-uri = "";
+        picture-uri-dark = "";
+        primary-color = "#000000";
+        secondary-color = "#000000";
+      };
+      "org/gnome/desktop/input-sources" = {
+        sources = "[('xkb', 'us')]";
+      };
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+        accent-color = "orange";
+        show-battery-percentage = true;
+      };
+      "org/gnome/desktop/peripherals/keyboard" = {
+        delay = 300;
+        repeat-interval = 14;
+      };
+      "org/gnome/desktop/peripherals/mouse" = {
+        speed = -0.3;
+      };
+      "org/gnome/desktop/screensaver" = {
+        color-shading-type = "solid";
+        picture-options = "zoom";
+        picture-uri = "";
+        primary-color = "#000000";
+        secondary-color = "#000000";
+      };
+      "org/gnome/desktop/session" = {
+        idle-delay = 0;
+      };
+      "org/gnome/evolution-data-server" = {
+        migrated = true;
+      };
+      "org/gnome/mutter" = {
+        edge-tiling = false;
+      };
+      "org/gnome/mutter/keybindings" = {
+        toggle-tiled-left = "@as []";
+        toggle-tiled-right = "@as []";
+      };
+      "org/gnome/nautilus/preferences" = {
+        default-folder-viewer = "icon-view";
+        migrated-gtk-settings = true;
+        search-filter-time-type = "last_modified";
+        show-hidden-files = true;
+      };
+      "org/gnome/nautilus/window-state" = {
+        initial-size = "(890, 550)";
+      };
+      "org/gnome/settings-daemon/plugins/power" = {
+        ambient-enabled = false;
+        idle-dim = false;
+        sleep-inactive-ac-timeout = 3600;
+        sleep-inactive-ac-type = "nothing";
+        sleep-inactive-battery-type = "nothing";
+      };
+      "org/gnome/shell" = {
+        favorite-apps = ["org.gnome.Nautilus.desktop" "google-chrome.desktop" "dev.zed.Zed.desktop" "com.mitchellh.ghostty.desktop" "tableplus.desktop"];
+        welcome-dialog-last-shown-version = "47.0";
+        enabled-extensions = ["dash-to-dock@micxgx.gmail.com"];
+      };
+      "org/gnome/shell/extensions/dash-to-dock" = {
+        dock-position = "LEFT";
+        intellihide = false;
+        dash-max-icon-size = 50;
+        show-apps-always-in-the-edge = true;
+        show-mounts = false;
+        show-show-apps-button = false;
+        show-trash = false;
+      };
+      "org/gnome/shell/extensions/ding" = {
+        check-x11wayland = true;
+        show-home = false;
+      };
+      "org/gnome/shell/extensions/tiling-assistant" = {
+        active-window-hint-color = "rgb(211,70,21)";
+        last-version-installed = 48;
+        overridden-settings = "{'org.gnome.mutter.edge-tiling': <@mb nothing>, 'org.gnome.mutter.keybindings.toggle-tiled-left': <@mb nothing>, 'org.gnome.mutter.keybindings.toggle-tiled-right': <@mb nothing>}";
+        tiling-popup-all-workspace = true;
+      };
+      "org/gnome/shell/ubuntu" = {
+        startup-sound = "";
+      };
+      "org/gnome/shell/world-clocks" = {
+        locations = "@av []";
+      };
+      "org/gtk/gtk4/settings/file-chooser" = {
+        show-hidden = false;
+        sort-directories-first = true;
+      };
+    };
+  };
+
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnsupportedSystem = true;
+    };
+  };
+}

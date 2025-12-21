dd7aff85e95fa05993515209edd6428fe10f3cd0
Joost de Lange <post.joostdelange@gmail.com>
2025-12-21 22:25:33 +0100

    feat: add aws cli install

--- install.sh
+++ install.sh
@@ -75,6 +75,13 @@ case \":\$PATH:\" in
 esac" >> ~/.zshrc
 fi
 
+if ! command -v aws >/dev/null 2>&1; then
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  sudo ./aws/install
+  rm -rf aws awscliv2.zip
+fi
+
 if ! command -v mullvad >/dev/null 2>&1; then
   log "Installing Mullvad VPN..."
   sudo curl -fsSLo "$KEYRINGS_DIR/mullvad-keyring.asc" https://repository.mullvad.net/deb/mullvad-keyring.asc
